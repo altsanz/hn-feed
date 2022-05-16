af594b8c326e6c2cac9d70f780a622c746db6142
Alejandro Sanz Díaz <alejandrosanzdiaz@Alejandros-MacBook-Pro.local>
2022-05-16 21:17:48 +0200

    add slice for hackernews with basic search

---
+++ src/services/hn/hn.slice.ts
@@ -0,0 +1,34 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+export interface HnItem {
+    title: string;
+    author: string;
+    objectId: number;
+    num_of_comments: number;
+}
+
+export interface HnList {
+    hits: HnItem[];
+    page: number;
+}
+
+export interface HnSearchPayload {
+    page: number;
+    query: string;
+}
+
+
+export const hnApi = createApi({
+    reducerPath: 'hackerNewsApi',
+    baseQuery: fetchBaseQuery({ baseUrl: 'https://hn.algolia.com/api/v1' }),
+    endpoints: (builder) => ({
+        searchPosts: builder.query<HnList, HnSearchPayload>({
+            query: () => {
+                return '/search'
+            }
+        })
+    })
+})
+
+
+export const {  useSearchPostsQuery } = hnApi;
