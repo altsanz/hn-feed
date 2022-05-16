91783110c9496fb5700edcd058ff69377167e936
Alejandro Sanz Díaz <alejandrosanzdiaz@Alejandros-MacBook-Pro.local>
2022-05-16 21:23:21 +0200

    connect rtk slice with store and render bare list

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 
 import './App.css';
+import { useSearchPostsQuery } from './services/hn/hn.slice';
 
 function App() {
+
+  const { data, isLoading } = useSearchPostsQuery({ page: 0, query: ''});
   return (
     <div className="App">
       <header className="App-header">
        Hacker News feed
       </header>
+
+      <div>
+        <pre>{ isLoading ? 'loading' : JSON.stringify(data?.hits) }</pre>
+      </div>
     </div>
   );
 }
--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,15 @@
+
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import { hnApi } from '../services/hn/hn.slice';
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    [hnApi.reducerPath]: hnApi.reducer
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(hnApi.middleware),
 });
 
 export type AppDispatch = typeof store.dispatch;
