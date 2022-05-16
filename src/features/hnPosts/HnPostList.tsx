0607f0b61a0e4c31a752280136c84e4052c954e9
Alejandro Sanz Díaz <alejandrosanzdiaz@Alejandros-MacBook-Pro.local>
2022-05-16 22:08:52 +0200

    add posts at the end when asking for another page

--- src/features/hnPosts/HnPostList.tsx
+++ src/features/hnPosts/HnPostList.tsx
@@ -1,19 +1,33 @@
 import { Box, Button, Grid } from "@mui/material";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useSearchPostsQuery } from "../../services/hn/hn.slice";
+import { HnItem } from "../../services/hn/hn.types";
 import HnPostCard from "./HnPostCard";
 
 export const HnPostList = () => {
   const [page, setPage] = useState(0);
-  const { data: postList } = useSearchPostsQuery({ page, query: "" });
+  const { data: fetchedPostList } = useSearchPostsQuery({ page, query: "" });
+  const [shownPostList, setShownPostList] = useState<HnItem[]>([]);
   const incrementPage = () => {
     setPage(page + 1);
   };
+
+  useEffect(() => {
+    if (!fetchedPostList?.hits) return;
+    if (page === 0) {
+      setShownPostList(fetchedPostList.hits);
+      return;
+    }
+
+    setShownPostList([...shownPostList, ...fetchedPostList.hits]);
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [fetchedPostList]);
   return (
     <Box sx={{ display: "flex", flexDirection: "column", margin: "0 25px" }}>
       <Grid container spacing={2}>
-        {postList &&
-          postList.hits.map((post) => (
+        {shownPostList &&
+          shownPostList.map((post) => (
             <Grid item xs={12} sm={6} md={4} alignItems="stretch">
               <HnPostCard post={post} key={post.objectID}></HnPostCard>
             </Grid>
