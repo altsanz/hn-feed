a7b820b941ed9475f0ffcdc738b8da259e9e9e57
Alejandro Sanz Díaz <alejandrosanzdiaz@Alejandros-MacBook-Pro.local>
2022-05-16 22:32:55 +0200

    remove counter default folder

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,12 +1,9 @@
-
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
-import { hnApi } from '../services/hn/hn.slice';
+import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
+import { hnApi } from "../services/hn/hn.slice";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
-    [hnApi.reducerPath]: hnApi.reducer
+    [hnApi.reducerPath]: hnApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(hnApi.middleware),
