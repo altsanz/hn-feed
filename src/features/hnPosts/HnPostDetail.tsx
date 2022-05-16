a456b77c3c275fb66128755d6978583e02af3c4c
Alejandro Sanz Díaz <alejandrosanzdiaz@Alejandros-MacBook-Pro.local>
2022-05-16 23:41:41 +0200

    navigate to post detail

--- src/features/hnPosts/HnPostDetail.tsx
+++ src/features/hnPosts/HnPostDetail.tsx
@@ -1,6 +1,18 @@
 import { useParams } from "react-router-dom";
 
 export const HnPostDetail = () => {
-  const params = useParams();
-  return <div>{JSON.stringify(params.postId)}</div>;
+  const { postId } = useParams();
+  //   const navigate = useNavigate();
+  //   const [postIdInvalid, setPostIdInvalid] = useState<boolean>(true);
+  //   const [parsedPostId, setParsedPostId] = useState(0);
+  //   const { data: post } = useGetPostByIdQuery(parsedPostId, {
+  //     skip: postIdInvalid,
+  //   });
+  //   if (postId) {
+  //     setParsedPostId(parseInt(postId, 10));
+  //     setPostIdInvalid(false);
+  //   } else {
+  //     navigate("/");
+  //   }
+  return <div>{postId && JSON.stringify(postId)}</div>;
 };
--- src/services/hn/hn.slice.ts
+++ src/services/hn/hn.slice.ts
@@ -29,4 +29,4 @@ export const hnApi = createApi({
   }),
 });
 
-export const { useSearchPostsQuery } = hnApi;
+export const { useSearchPostsQuery, useGetPostByIdQuery } = hnApi;
